74243dd30ee37798e41a0cc981cfcdea975369bf
abbypan <abbypan@gmail.com>
2016-05-21 19:11:17 +0800

    next page

--- lofter.pl
+++ lofter.pl
@@ -27,60 +27,77 @@ unless ( defined $writer and defined $book ) {
     exit;
 }
 
-my $BASE_URL = "http://$writer.lofter.com";
-$b = uc( unpack( "H*", encode( "utf8", $book ) ) );
-$b =~ s/(..)/%$1/g;
-my $url = "$BASE_URL/search/?q=$b";
-print "url : $url\n";
+my $output = encode( locale => "$writer-$book.txt" );
 
-my $xs = Novel::Robot->new( site => 'txt', type => 'txt' );
-my @floor;
-my $i = 1;
-while (1) {
-    last if ( $pg_max_n and $i > $pg_max_n );
-    print "extract chapter url : $url\n";
-    my $h = $xs->{browser}->request_url($url);
-    my $r = scraper {
-        process '//h2[@class="ttl"]//a',
-          'chapter[]' => {
-            title => 'TEXT',
-            url   => '@href'
-          };
-        process_first '//a[@class="next "]',
-          'next' => {
-            title => 'TEXT',
-            url   => '@href'
-          };
-    };
-    my $chap_r = $r->scrape($h);
-    push @floor, @{ $chap_r->{chapter} };
-    last unless ( $chap_r->{next} and $chap_r->{next}{url} );
-    $url = "$BASE_URL$chap_r->{next}{url}";
-    $i++;
+get_lofter_book( { writer => $writer, book => $book, txt => $output } );
+
+sub get_lofter_book {
+    my ($opt)  = @_;
+    my $writer = $opt->{writer};
+    my $book   = $opt->{book};
+
+    my $BASE_URL = "http://$writer.lofter.com";
+    $b = uc( unpack( "H*", encode( "utf8", $book ) ) );
+    $b =~ s/(..)/%$1/g;
+    my $url = "$BASE_URL/search/?q=$b";
+
+    my $xs = Novel::Robot->new( site => 'txt', type => 'txt' );
+    my @floor;
+    my $i = 1;
+    my $u = $url;
+    while (1) {
+        last if ( $opt->{max_page_num} and $i > $opt->{max_page_num} );
+        my $h = $xs->{browser}->request_url($u);
+        my $r = scraper {
+            process '//h2[@class="ttl"]//a',
+              'chapter[]' => {
+                title => 'TEXT',
+                url   => '@href'
+              };
+            result 'chapter';
+        };
+        my $chap_r = $r->scrape($h);
+        last unless ( $chap_r and @$chap_r );
+        push @floor, @$chap_r;
+        $i++;
+        $u = "$url&page=$i";
+    }
+    @floor = reverse @floor;
+    $xs->{parser}->update_url_list( \@floor, $BASE_URL );
+
+    for my $x (@floor) {
+        my $u = $x->{url};
+        my $h = $xs->{browser}->request_url($u);
+        my $r = scraper {
+            process '//div[@class="txtcont"]', 'content' => 'HTML';
+            result 'content';
+        };
+        $x->{content} = $r->scrape($h);
+    }
+
+    my %book_data = (
+        writer     => $writer,
+        book       => $book,
+        floor_list => \@floor,
+        url        => $url,
+    );
+
+    if ( $opt->{txt} ) {
+        $xs->{packer}->main(
+            \%book_data,
+            output   => $opt->{txt},
+            with_toc => 0
+        );
+        return $opt->{txt};
+    }
+
+    my $d = '';
+    $xs->{packer}->main(
+        \%book_data,
+        output   => \$d,
+        with_toc => 0
+    );
+    $d =~ s/\n/\r\n/sg;
+    $book_data{data} = $d;
+    return \%book_data;
 }
-@floor = reverse @floor;
-$xs->{parser}->update_url_list( \@floor, $BASE_URL );
-
-for my $x (@floor) {
-    my $u = $x->{url};
-    print "download chapter : $u\n";
-    my $h = $xs->{browser}->request_url($u);
-    my $r = scraper {
-        process '//div[@class="txtcont"]', 'content' => 'HTML';
-        result 'content';
-    };
-    $x->{content} = $r->scrape($h);
-}
-
-my %book_data = (
-    writer     => $writer,
-    book       => $book,
-    floor_list => \@floor,
-    url        => $url,
-);
-
-$xs->{packer}->main(
-    \%book_data,
-    output   => encode( locale => "$writer-$book.txt" ),
-    with_toc => 0
-);
