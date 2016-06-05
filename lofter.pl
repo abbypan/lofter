ca62de01704234ab85c2ec6b9142864f2f8340dd
abbypan <abbypan@gmail.com>
2016-06-05 14:50:58 +0800

    use get_tiezi_ref

--- lofter.pl
+++ lofter.pl
@@ -16,13 +16,13 @@ binmode( STDIN,  ":encoding(console_in)" );
 binmode( STDOUT, ":encoding(console_out)" );
 binmode( STDERR, ":encoding(console_out)" );
 
-my ( $writer, $book, $pg_max_n ) = @ARGV;
+my ( $writer, $book) = @ARGV;
 
 $writer = decode( locale => $writer );
 $book   = decode( locale => $book );
 unless ( defined $writer and defined $book ) {
     print "example:
-    perl lofter.pl 'chuweizhiyu' '时之足' 30
+    perl lofter.pl 'chuweizhiyu' '时之足'
     \n";
     exit;
 }
@@ -42,49 +42,51 @@ sub get_lofter_book {
     my $url = "$BASE_URL/search/?q=$b";
 
     my $xs = Novel::Robot->new( site => 'txt', type => 'txt' );
-    my @floor;
-    my $i = 1;
-    my $u = $url;
-    while (1) {
-        last if ( $opt->{max_page_num} and $i > $opt->{max_page_num} );
-        my $h = $xs->{browser}->request_url($u);
-        my $r = scraper {
-            process '//h2[@class="ttl"]//a',
-              'chapter[]' => {
-                title => 'TEXT',
-                url   => '@href'
-              };
-            result 'chapter';
-        };
-        my $chap_r = $r->scrape($h);
-        last unless ( $chap_r and @$chap_r );
-        push @floor, @$chap_r;
-        $i++;
-        $u = "$url&page=$i";
-    }
-    @floor = reverse @floor;
-    $xs->{parser}->update_url_list( \@floor, $BASE_URL );
 
-    for my $x (@floor) {
-        my $u = $x->{url};
-        my $h = $xs->{browser}->request_url($u);
-        my $r = scraper {
-            process '//div[@class="txtcont"]', 'content' => 'HTML';
-            result 'content';
-        };
-        $x->{content} = $r->scrape($h);
-    }
-
-    my %book_data = (
-        writer     => $writer,
-        book       => $book,
-        floor_list => \@floor,
-        url        => $url,
+    my $post_r = $xs->{parser}->get_tiezi_ref($url,
+        parse_info => sub { { writer=>$writer, book=>$book, title=>$book } },
+        parse_content => sub {
+            my ($h) = @_;
+            my $r = scraper {
+                process '//h2//a',
+                'chapter[]' => {
+                    title => 'TEXT',
+                    url   => '@href'
+                };
+                result 'chapter';
+            };
+            my $chap_r = $r->scrape($h);
+            return unless ( $chap_r and @$chap_r );
+            my @chap_tidy = grep { $_->{title}=~/$book/
+                and $_->{url}=~m#/post/# } @$chap_r;
+            return \@chap_tidy;
+        }, 
+        #min_page_num=>2, 
+        #max_page_num => 3, 
+        #stop_iter=>sub {
+        #my ($info, $data_list, $i, %o) = @_;
+        #return 1 if($i>4);
+        #},
+        next_url => sub {
+            my ($start_u, $i, $h) = @_;
+            return "$start_u&page=$i";
+        }, 
+        deal_content_url => sub {
+            my ($h) = @_;
+            my $r = scraper {
+                process '//div[@class="txtcont"]', 'content' => 'HTML';
+                process '//div[@class="content"]', 'cont2' => 'HTML';
+            };
+            my $res=$r->scrape($h);
+            return $res->{content} || $res->{cont2};
+        }, 
+        reverse_content_list => 1, 
     );
 
     if ( $opt->{txt} ) {
         $xs->{packer}->main(
-            \%book_data,
+            #\%book_data,
+            $post_r, 
             output   => $opt->{txt},
             with_toc => 0
         );
@@ -93,11 +95,11 @@ sub get_lofter_book {
 
     my $d = '';
     $xs->{packer}->main(
-        \%book_data,
+        $post_r, 
         output   => \$d,
         with_toc => 0
     );
     $d =~ s/\n/\r\n/sg;
-    $book_data{data} = $d;
-    return \%book_data;
+    $post_r->{data} = $d;
+    return $post_r, 
 }
